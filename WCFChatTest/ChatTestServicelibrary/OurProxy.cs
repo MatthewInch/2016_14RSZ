a2528d590dfec6652e2d2e5c5fb1a3b715c89c24
steamhunter <steamhunter97@gmail.com>
2016-12-13 10:23:17 +0100

    orai munka

---
+++ WCFChatTest/ChatTestServicelibrary/OurProxy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatTestServicelibrary
+{
+    public class OurProxy
+    {
+        private static readonly OurProxy _instance = new OurProxy();
+
+        private OurProxy()
+        {
+        }
+
+        public static OurProxy GetProxy()
+        {
+            return _instance;
+        }
+
+        public string StartMessage { get; set; }
+    }
+}
