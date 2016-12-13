1c82713d41467238faca16c2fcfcb6f88789fea8
asd <asd@ASDF>
2016-12-13 08:53:13 +0100

    Add project files.

---
+++ ChatTest/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ChatTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(13, 13);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(309, 20);
+            this.txtText.TabIndex = 0;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(13, 40);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 522);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ ChatTest/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatTest
+{
+    public partial class Form1 : Form
+    {
+        private ServiceHost _host;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            using (ChatServiceReference.Service1Client client = new ChatServiceReference.Service1Client())
+            {
+                //txtText.Text = client.CountALetter(txtText.Text).ToString();
+                txtText.Text = client.GetDefaultMessage();
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                _host = new ServiceHost(typeof(ChatTestServicelibrary.Service1));
+                _host.Open();
+            });
+        }
+    }
+}
